656f6e4855b2177ffcac40ef37a4f160d28cef22
Nicolas Celik <nicolas.celik@epita.fr>
2023-11-04 20:36:44 +0100

    docs(ip): add documentation for exported function

--- libs/image_processing/include/image_processing.h
+++ libs/image_processing/include/image_processing.h
@@ -3,6 +3,12 @@
 
 #include <stdbool.h>
 
+/// Reads an image and extracts sudoku cells from it.
+/// \param image_path Path to source image file.
+/// \param cell_path_fmt Format of paths to sudoku cell images to write.
+/// \param rotate_degrees Number of degrees to rotate the source image by before
+/// actual processing.
+/// \return True on success, or false on failure.
 bool ip_process_image(
     const char *image_path,
     const char *cell_path_fmt,
